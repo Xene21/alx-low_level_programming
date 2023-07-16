abb8a280aff49c23a7c00e065e7fd251c340f547
Xene21 <137602696+Xene21@users.noreply.github.com>
2023-07-16 13:58:38 -0700

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,33 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
-
 /**
-
- * main - Prints numbers from 0 to 9.
-
+ * main - Entry
  *
-
- * Return: Always 0 (Success)
-
+ * Return: Always 0
  */
 
 int main(void)
-
 {
-
-        int i;
-
-
-        for (i = 48; i < 58; i++)
-
-        {
-
-                putchar(i);
-
-        }
-
-        putchar('\n');
-
-        return (0);
-
+int num;
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
 }
